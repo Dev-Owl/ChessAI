e282ee3d0ac8d9990ec69887e74825f69e802cf4
Christian <c.muehle@googlemail.com>
2014-02-21 16:35:23 +0100

    Added RichardRandom
    Added the project files and base types for the RichardRandom AI

---
+++ RichardRandom/RichardRandom.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ABChess.Engine;
+
+namespace AI
+{
+    public class RichardRandom : IAI
+    {
+        public RichardRandom()
+        { 
+        
+        }
+
+        #region IAI Members
+
+        public void Init()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void YourTurn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ForceMove()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MatchEnding()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IPromotion Members
+
+        public EFigures GetDecision()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
